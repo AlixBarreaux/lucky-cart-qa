359816ccf6a8a30be52c2f283b131d1f8c269709
AlixBarreaux <alixbarreaux@tutanota.com>
2026-04-27 19:13:37 +0200

    style: Change single quotes to double quotes
    Consistency accross TS files.

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   timeout: 30000,
   retries: 0,
+  reporter: [["html", { open: "never" }]],
   use: {
     headless: true,
-    screenshot: 'only-on-failure',
-    video: 'off',
+    screenshot: "only-on-failure",
+    video: "off",
   },
   projects: [
     {
-      name: 'chromium',
-      use: { browserName: 'chromium' },
+      name: "chromium",
+      use: { browserName: "chromium" },
     },
   ],
 });
